dc3e1658f5e8766f668d9f29f2a0bddf29df10e5
Prabidush <prabidushkumarlal@gmail.com>
2025-05-31 16:36:22 +0530

    server.js updated for vercel

--- ChatApp/server/server.js
+++ ChatApp/server/server.js
@@ -68,6 +68,12 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
+
 await connectDB(); // Connect to MongoDB
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV!=="production"){
+    const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+//export server for vercel
+export default server;
